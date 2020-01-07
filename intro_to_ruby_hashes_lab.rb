d18fec211b11103abb62eab438302dcee5d7c692
Ben Taylor <ben10taylor10@hotmail.com>
2020-01-07 11:15:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,12 +15,12 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-  p {id: 9}
+  p {:id => 9}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  p {key: value}
+  p {:key => value}
 end
 
 def read_from_hash(hash, key)
